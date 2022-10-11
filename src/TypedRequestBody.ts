ff9806a084f54973ec91f8e6aa5a03918b4112c7
MarianChoma <xchoma@stuba.sk>
2022-10-11 18:08:20 +0200

    upload code

--- src/TypedRequestBody.ts
+++
@@ -1,8 +0,0 @@
-import {IncomingHttpHeaders} from "http";
-
-export interface TypedRequestBody<T> extends Express.Request {
-    body: T,
-    headers: IncomingHttpHeaders & {
-        accesstoken?: String
-    }
-}
